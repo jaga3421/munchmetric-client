8c1f333470e8c2211580ecbc38a8786f17d4f6ce
jaga3421 <jagadeesh.jkp@gmail.com>
2023-12-10 09:06:20 +0530

    Update package.json and HeroAnimation component

--- src/components/HeroAnimation.jsx
+++ src/components/HeroAnimation.jsx
@@ -1,10 +1,20 @@
 // Lotte files
 import Lottie from "lottie-react";
-import animationData from "../animations/hero-bike.json";
+
+import animationData1 from "../animations/hero-bike.json";
+import animationData2 from "../animations/hero-next.json";
+import animationData3 from "../animations/hero-line.json";
 
 function HeroAnimation() {
+  const animationData =
+    Math.random() < 0.33
+      ? animationData1
+      : Math.random() < 0.5
+      ? animationData2
+      : animationData3;
+
   return (
-    <div className="hero-animation-container">
+    <div className="hero-animation-container max-h-[400px]">
       <Lottie animationData={animationData} />
     </div>
   );
